795d3f8851ffdfdfe1e518e3d5a3e469fc25f997
arvapu <arvydas.apulskis123@gmail.com>
2020-10-01 21:45:27 +0300

    endpoints fixed

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ $router->get('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments', 'CommentCont
 
 /* Get one */
 $router->get('/users/{userId:\d+}', ['as' => 'get_user', 'uses' => 'UserController@one']);
-$router->get('/users/{userId:\d+}/recipes/{recipeId:\id}', ['as' => 'get_recipe', 'uses' => 'RecipeController@oneForUser']);
+$router->get('/users/{userId:\d+}/recipes/{recipeId:\d+}', ['as' => 'get_recipe', 'uses' => 'RecipeController@oneForUser']);
 $router->get('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments/{commentId:\d+}', ['as' => 'get_comment', 'uses' => 'CommentController@oneForUserRecipe']);
 
 /* Create */
@@ -18,9 +18,9 @@ $router->post('/users/{userId:\d+}/recipes', 'RecipeController@createForUser');
 $router->post('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments', 'CommentController@createForUserRecipe');
 
 /* Edit */
-$router->put('/users/{userId:\d+}', 'UserController@update');
-$router->put('/users/{userId:\d+}/recipes/{recipeId:\d+}', 'RecipeController@updateForUser');
-$router->put('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments/{commentId:\d+}', 'CommentController@updateForUserRecipe');
+$router->patch('/users/{userId:\d+}', 'UserController@update');
+$router->patch('/users/{userId:\d+}/recipes/{recipeId:\d+}', 'RecipeController@updateForUser');
+$router->patch('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments/{commentId:\d+}', 'CommentController@updateForUserRecipe');
 
 /* Delete */
 $router->delete('/users/{userId:\d+}', 'UserController@destroy');
