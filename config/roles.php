b3089f46d31959577aecda10ec91315e2546b13b
arvapu <arvydas.apulskis123@gmail.com>
2020-10-19 20:08:06 +0300

    roles migration added

---
+++ config/roles.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Roles
+    |--------------------------------------------------------------------------
+    |
+    | List of available roles in system
+    |
+    */
+
+    ['id' => 'admin', 'description' => 'System admin\'s role.'],
+    ['id' => 'user', 'description' => 'Simple user\'s role.'],
+];
