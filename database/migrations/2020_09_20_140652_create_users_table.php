532d0b482f35dab3c3265e099a402075f9191b97
arvapu <arvydas.apulskis123@gmail.com>
2020-10-19 19:58:58 +0300

    users migration updated

--- database/migrations/2020_09_20_140652_create_users_table.php
+++ database/migrations/2020_09_20_140652_create_users_table.php
@@ -15,8 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email', 140)->unique();
-            $table->string('username', 60)->unique()->nullable();
+            $table->unsignedBigInteger('oauth_user_id');
+            $table->string('role_id');
+            $table->string('email', 140);
+            $table->string('username', 60)->nullable();
             $table->timestamp('last_login_at')->nullable()->default(null);
             $table->timestamps();
             $table->softDeletes();
