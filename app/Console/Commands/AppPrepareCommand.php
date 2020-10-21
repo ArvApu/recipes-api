808ee79ada12de3f4f49a05fd8c0b0f13c998ba5
arvapu <arvydas.apulskis123@gmail.com>
2020-10-21 15:05:49 +0300

    app prepare command added

---
+++ app/Console/Commands/AppPrepareCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AppPrepareCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string $signature
+     */
+    protected $signature = 'app:prepare';
+
+    /**
+     * The console command description.
+     *
+     * @var string $description
+     */
+    protected $description = 'Execute needed commands to start program.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->call('migrate', ['--force' => true]);
+    }
+}
