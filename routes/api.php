49564150b9cc6008e66b2da4d241cfb1be5ea998
arvapu <arvydas.apulskis123@gmail.com>
2020-11-26 12:57:36 +0200

    minor changes

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,18 @@ $router->group([
     /* Get all recipes that can be long to any user */
     $router->get('/recipes', 'RecipeController@all');
 
+    /* Get single recipe that belongs to specific user */
+    $router->get('/users/{userId:\d+}/recipes/{recipeId:\d+}', ['as' => 'get_recipe', 'uses' => 'RecipeController@oneForUser']);
+
+    /* Create recipe's comment */
+    $router->post('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments', 'CommentController@createForUserRecipe');
+
+    /* Update recipe's comment */
+    $router->patch('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments/{commentId:\d+}', 'CommentController@updateForUserRecipe');
+
+    /* Delete recipe's comment */
+    $router->delete('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments/{commentId:\d+}', 'CommentController@destroyForUserRecipe');
+
     $router->group(['middleware' => 'admin'], function ($router) {
 
         /* Get all */
@@ -33,22 +45,18 @@ $router->group([
         $router->get('/users/{userId:\d+}/recipes', 'RecipeController@allForUser');
 
         /* Get one */
-        $router->get('/users/{userId:\d+}/recipes/{recipeId:\d+}', ['as' => 'get_recipe', 'uses' => 'RecipeController@oneForUser']);
         $router->get('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments/{commentId:\d+}', ['as' => 'get_comment', 'uses' => 'CommentController@oneForUserRecipe']);
 
         /* Create */
         $router->post('/users/{userId:\d+}/recipes', 'RecipeController@createForUser');
-        $router->post('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments', 'CommentController@createForUserRecipe');
 
         /* Edit */
         $router->patch('/users/{userId:\d+}', 'UserController@update');
         $router->patch('/users/{userId:\d+}/recipes/{recipeId:\d+}', 'RecipeController@updateForUser');
-        $router->patch('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments/{commentId:\d+}', 'CommentController@updateForUserRecipe');
 
         /* Delete */
         $router->delete('/users/{userId:\d+}', 'UserController@destroy');
         $router->delete('/users/{userId:\d+}/recipes/{recipeId:\d+}', 'RecipeController@destroyForUser');
-        $router->delete('/users/{userId:\d+}/recipes/{recipeId:\d+}/comments/{commentId:\d+}', 'CommentController@destroyForUserRecipe');
     });
 });
 
